41b7e055176b385454b34c11546c470aeab8b7ed
POUPAE <psinsub@hks.harvard.edu>
2020-01-21 21:29:38 -0500

    add the bonus part to calculator.py

--- calculator.py
+++ calculator.py
@@ -14,7 +14,34 @@ def subtract(a,b):
 def divide(a,b):
     return a / b
 
+# square function
+def square(a):
+    return a ** 2
+
+# cube function
+def cube(a):
+    return a ** 3
+
+# square_n function
+# divide function
+def square_n(a,n):
+    return a ** n
+
+
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+print("I'm going use the calculator functions to print square of 5")
+x = square(5)
+print(x)
+
+print("I'm going use the calculator functions to print cube of 5")
+x = cube(5)
+print(x)
+
+print("I'm going use the calculator functions to print square n of 5")
+x = square_n(5,4)
+print(x)
+
 
