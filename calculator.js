30fd42a3c9f5d37f92734358ba532c6d3d51acc5
POUPAE <psinsub@hks.harvard.edu>
2020-01-21 21:32:19 -0500

    define functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// This part of the code defines multiple function
+function multiply(a,b) { 
+    return a * b
+}
+
+// This part of the code defines add function
+function add(a,b) { 
+    return a + b
+}
+
+// This part of the code defines subtract function
+function subtract(a,b) { 
+    return a - b
+}
+
+// This part of the code defines divide function
+function divide(a,b) { 
+    return a / b
+}
