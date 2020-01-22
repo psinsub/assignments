71cd7c2da4f2bc17212a119a8c6e999f8b5f8af3
POUPAE <psinsub@hks.harvard.edu>
2020-01-21 21:16:26 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# multiple function
+def multiply(a,b):
+    return a * b
+
+# add function
+def add(a,b):
+    return a + b
+
+# subtract function
+def multiply(a,b):
+    return a - b
+
+# divide function
+def multiply(a,b):
+    return a / b
+
+
